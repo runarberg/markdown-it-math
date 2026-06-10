954d0629abc70b258fe83b72dc6ed8b9954ed00e
Rúnar Berg <runarberg@zoho.com>
2026-06-09 20:50:54 -0700

    Fix demo page

--- scripts/build-pages.sh
+++ scripts/build-pages.sh
@@ -2,7 +2,8 @@
 
 mkdir -p pages
 cp -r demo pages/
-cp index.js pages/
+cp -r src pages/
+cp index.js temml.js no-default-renderer.js pages/
 
 mkdir -p pages/node_modules
 cp -r node_modules/{markdown-it,mathup,temml,linkify-it,entities,mdurl,punycode,uc.micro} pages/node_modules/
